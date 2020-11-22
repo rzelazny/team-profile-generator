0c23fe547df58d5d11e8bf36445de485f1f5a8e4
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-22 17:05:43 -0500

    added intern coffee test

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,18 +2,24 @@ const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Foo", 1, "test@test.com", testValue, "Genuine barista");
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA", "Genuine barista");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Foo", 1, "test@test.com", testValue, "Genuine barista");
   expect(e.getSchool()).toBe(testValue);
+});
+
+test("Can get coffee brewing ability via getBrewAbility()", () => {
+  const testValue = "Genuine barista";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA", testValue);
+  expect(e.getBrewAbility()).toBe(testValue);
 });
