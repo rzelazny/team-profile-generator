588010dac5335677b390eea598e8403b5aac3ef5
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-22 17:03:30 -0500

    additional engineer test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -2,18 +2,24 @@ const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer("Foo", 1, "test@test.com", testValue, "555-555-1234");
   expect(e.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser", "555-555-1234");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer("Foo", 1, "test@test.com", testValue, "555-555-1234");
   expect(e.getGithub()).toBe(testValue);
+});
+
+test("Can get phone number via getPhone()", () => {
+  const testValue = "555-555-1234";
+  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser", testValue);
+  expect(e.getPhone()).toBe(testValue);
 });
