89b9ab344f404508c206cdad3fd2a4a8213da877
Ryan Zelazny <RyanZelazny@gmail.com>
2020-11-22 17:07:30 -0500

    added manager coffee test

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -3,18 +3,24 @@ const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const e = new Manager("Foo", 1, "test@test.com", testValue, "black");
   expect(e.officeNumber).toBe(testValue);
 });
 
 test('getRole() should return "Manager"', () => {
   const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  const e = new Manager("Foo", 1, "test@test.com", 100, "black");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const e = new Manager("Foo", 1, "test@test.com", testValue, "black");
   expect(e.getOfficeNumber()).toBe(testValue);
+});
+
+test("Can get coffee preference via getCoffeePreference()", () => {
+  const testValue = "black";
+  const e = new Manager("Foo", 1, "test@test.com", 100, testValue);
+  expect(e.getCoffeePreference()).toBe(testValue);
 });
